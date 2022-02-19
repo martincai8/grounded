34f7b98c6f6730601edea196a61c81f7a2852b66
Martin Cai <martinc8@student.ubc.ca>
2022-02-19 14:23:11 -0800

    added automatic redirection once circles are filled

--- static/js/senses.js
+++ static/js/senses.js
@@ -4,44 +4,57 @@ let option3 = document.getElementById("option3");
 let option4 = document.getElementById("option4");
 let option5 = document.getElementById("option5");
 
-function selectOption1() {
+function selectOption1(moveOn) {
     let option1 = document.getElementById("option1");
     option1.classList.add("option-selected");
-
+    if (moveOn) {
+        window.location.href = '/resources';
+    }
 }
 
-function selectOption2() {
+function selectOption2(moveOn) {
     let option1 = document.getElementById("option1");
     if (option1.classList.contains("option-selected")) {
         let option2 = document.getElementById("option2");
         option2.classList.add("option-selected");
-
+        if (moveOn) {
+            window.location.href = '/senses/1';
+        }
     }
 }
 
-function selectOption3() {
+function selectOption3(moveOn) {
     let option2 = document.getElementById("option2");
     if (option2.classList.contains("option-selected")) {
         let option3 = document.getElementById("option3");
         option3.classList.add("option-selected");
+        if (moveOn) {
+            window.location.href = '/senses/2';
+        }
     }
 
 }
 
-function selectOption4() {
+function selectOption4(moveOn) {
     let option3 = document.getElementById("option3");
     if (option3.classList.contains("option-selected")) {
         let option4 = document.getElementById("option4");
         option4.classList.add("option-selected");
+        if (moveOn) {
+            window.location.href = '/senses/3';
+        }
     }
 
 }
 
-function selectOption5() {
+function selectOption5(moveOn) {
     let option4 = document.getElementById("option4");
     if (option4.classList.contains("option-selected")) {
         let option5 = document.getElementById("option5");
         option5.classList.add("option-selected");
+        if (moveOn) {
+            window.location.href = '/senses/4';
+        }
     }
 }
 
