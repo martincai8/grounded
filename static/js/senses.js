b97808ae0ab89239f981af96c7280f6ce1685594
Sam <samz273@students.cs.ubc.ca>
2022-02-19 15:29:57 -0800

    changed selection functionality

--- static/js/senses.js
+++ static/js/senses.js
@@ -6,7 +6,9 @@ let option5 = document.getElementById("option5");
 
 function selectOption1(moveOn) {
     let option1 = document.getElementById("option1");
+    let option2 = document.getElementById("option2");
     option1.classList.add("option-selected");
+    option2.classList.add("option-to-select");
     if (moveOn) {
         window.location.href = '/resources';
     }
@@ -14,9 +16,11 @@ function selectOption1(moveOn) {
 
 function selectOption2(moveOn) {
     let option1 = document.getElementById("option1");
+    let option3 = document.getElementById("option3");
     if (option1.classList.contains("option-selected")) {
         let option2 = document.getElementById("option2");
         option2.classList.add("option-selected");
+        option3.classList.add("option-to-select");
         if (moveOn) {
             window.location.href = '/senses/1';
         }
@@ -24,10 +28,12 @@ function selectOption2(moveOn) {
 }
 
 function selectOption3(moveOn) {
+    let option4 = document.getElementById("option4");
     let option2 = document.getElementById("option2");
     if (option2.classList.contains("option-selected")) {
         let option3 = document.getElementById("option3");
         option3.classList.add("option-selected");
+        option4.classList.add("option-to-select");
         if (moveOn) {
             window.location.href = '/senses/2';
         }
@@ -36,10 +42,12 @@ function selectOption3(moveOn) {
 }
 
 function selectOption4(moveOn) {
+    let option5 = document.getElementById("option5");
     let option3 = document.getElementById("option3");
     if (option3.classList.contains("option-selected")) {
         let option4 = document.getElementById("option4");
         option4.classList.add("option-selected");
+        option5.classList.add("option-to-select");
         if (moveOn) {
             window.location.href = '/senses/3';
         }
