a7ec8efdded0dbc23e4b95b9ac9289bb1a26ff12
Ed <edwinchow1110@gmail.com>
2022-02-18 23:04:24 -0800

    added transitions on breathing page

--- static/frontend/js files/breathing.js
+++ static/frontend/js files/breathing.js
@@ -1,1 +1,27 @@
-const 
+const circle = document.querySelector('.circle');
+const instructions = document.querySelector('.main-text');
+const button = document.querySelector('.garbage');
+
+// change opacity of circle
+const changeCircle = () => {
+    circle.classList.add("dark-circle");
+    setTimeout(() => {
+        circle.classList.remove("dark-circle");
+    }, 8000);
+};
+
+const textChange = () => {
+    instructions.innerText = "breathe in";
+    setTimeout(() => {
+        instructions.innerText = "hold";
+        setTimeout(() => {
+            instructions.innerText = "slowly breathe out";
+        }, 4000);
+    }, 4000);
+};
+
+button.addEventListener("click", () => {
+    changeCircle();
+    textChange();
+});
+
