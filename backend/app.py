e375a21092a908a70f60cd6bab2101e9ad614326
Martin Cai <martinc8@student.ubc.ca>
2022-02-19 16:39:19 -0800

    added jinja if to ending.html

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,43 @@
 from flask import *
 import os
 
-app = Flask(__name__, template_folder="../templates", static_folder="../static")
+app = Flask(__name__, template_folder="../templates",
+            static_folder="../static")
+
 
 @app.route("/")
 def index():
     return render_template("start.html")
 
+
 @app.route("/breathe")
 def breathe():
     return render_template("breathing.html")
+
 
 @app.route("/resources")
 def resources():
     return render_template("resources.html")
 
-@app.route("/senses/<int:page>", methods = ["GET"])
+
+@app.route("/sensesStart")
+def sensesStart():
+    return render_template("senses/senses-start.html")
+
+
+@app.route("/senses/<int:page>", methods=["GET"])
 def senses(page):
     return render_template("senses/senses{}.html".format(page))
 
+
+@app.route("/checkin")
+def checkin():
+    return render_template("ending.html", ending = False)
+
 @app.route("/ending")
 def ending():
-    return render_template("ending.html")
+    return render_template("ending.html", ending = True)
+
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
